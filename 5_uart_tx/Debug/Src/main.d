06231125a128e8805b1b92f81cccf656f9b32add
ShuvroSankar <sankarshuvro1010@gmail.com>
2025-03-26 23:45:10 +0600

    existing files

---
+++ 5_uart_tx/Debug/Src/main.d
@@ -0,0 +1,17 @@
+Src/main.o: ../Src/main.c \
+ D:/STM32_bare_metal_project/CHIP_headers/CMSIS/Device/ST/STM32F4xx/cmsis-device-f4/cmsis-device-f4-cdbad761857acedcdd07ece7939b4cb209ed826a/Include/stm32f4xx.h \
+ D:/STM32_bare_metal_project/CHIP_headers/CMSIS/Device/ST/STM32F4xx/cmsis-device-f4/cmsis-device-f4-cdbad761857acedcdd07ece7939b4cb209ed826a/Include/stm32f446xx.h \
+ D:/STM32_bare_metal_project/CHIP_headers/CMSIS/Include/core_cm4.h \
+ D:/STM32_bare_metal_project/CHIP_headers/CMSIS/Include/cmsis_version.h \
+ D:/STM32_bare_metal_project/CHIP_headers/CMSIS/Include/cmsis_compiler.h \
+ D:/STM32_bare_metal_project/CHIP_headers/CMSIS/Include/cmsis_gcc.h \
+ D:/STM32_bare_metal_project/CHIP_headers/CMSIS/Include/mpu_armv7.h \
+ D:/STM32_bare_metal_project/CHIP_headers/CMSIS/Device/ST/STM32F4xx/cmsis-device-f4/cmsis-device-f4-cdbad761857acedcdd07ece7939b4cb209ed826a/Include/system_stm32f4xx.h
+D:/STM32_bare_metal_project/CHIP_headers/CMSIS/Device/ST/STM32F4xx/cmsis-device-f4/cmsis-device-f4-cdbad761857acedcdd07ece7939b4cb209ed826a/Include/stm32f4xx.h:
+D:/STM32_bare_metal_project/CHIP_headers/CMSIS/Device/ST/STM32F4xx/cmsis-device-f4/cmsis-device-f4-cdbad761857acedcdd07ece7939b4cb209ed826a/Include/stm32f446xx.h:
+D:/STM32_bare_metal_project/CHIP_headers/CMSIS/Include/core_cm4.h:
+D:/STM32_bare_metal_project/CHIP_headers/CMSIS/Include/cmsis_version.h:
+D:/STM32_bare_metal_project/CHIP_headers/CMSIS/Include/cmsis_compiler.h:
+D:/STM32_bare_metal_project/CHIP_headers/CMSIS/Include/cmsis_gcc.h:
+D:/STM32_bare_metal_project/CHIP_headers/CMSIS/Include/mpu_armv7.h:
+D:/STM32_bare_metal_project/CHIP_headers/CMSIS/Device/ST/STM32F4xx/cmsis-device-f4/cmsis-device-f4-cdbad761857acedcdd07ece7939b4cb209ed826a/Include/system_stm32f4xx.h:
